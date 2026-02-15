f9c076a749a8172f623449ac2375089b1e536a53
Elise <eliseromerogoux@hotmail.com>
2026-02-15 19:54:29 +0100

    Le perso bouge et saute une seule fois

--- test1.py
+++ test1.py
@@ -29,21 +29,7 @@ class App:
             self.saute+=1
             if self.saute==1:
                 self.perso.saut()
-        if self.perso.collision(self.ptf) or self.perso.pos[1]==self.S.sol:
-#         if self.perso.pos[1]==self.S.sol or (self.perso.pos[1]+20==self.ptf.co[1] and self.perso.pos[0]==self.ptf.co[0]):
-            self.saute=0
-#         if (btnp(KEY_Z) or btnp(KEY_UP)) and (btnp(KEY_D) or btnp(KEY_RIGHT)):
-#             self.perso.saut_d()
-#         if (btnp(KEY_Z) or btnp(KEY_UP)) and (btnp(KEY_G) or btnp(KEY_LEFT)):
-#             self.perso.saut_g()
-        if self.perso.collision(self.ptf)==False and self.perso.pos[1]<=self.S.sol:
-            self.perso.pos[1]+=self.perso.descente
-#         for i in range(self.ptf.co[0],self.ptf.co[0]+16):
-#             if self.perso.pos[0]!=i and self.perso.pos[1]<self.S.sol:
-#                 self.perso.pos[1]+=self.perso.descente
-#         print('4',self.perso.pos[1])
-        #faire un test qui verifie si on est sur une plateforme, sinon on descend(gere la descente du saut et la gravité)
-        # créer une valeur, qui est- la somme des forces qui nous font descendre et monter et appliquer cette force
+
     def draw(self):
         load(self.image)
         cls(11)
