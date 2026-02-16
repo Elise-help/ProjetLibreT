76dcedf7f2966e04c048436e0d3fb3665aec9b5d
Elise <eliseromerogoux@hotmail.com>
2026-02-16 07:26:23 +0100

    Version 2

--- class_perso.py
+++ class_perso.py
@@ -8,24 +8,22 @@ class Personnage:
         self.nv=nv
         self.h=0
         self.s=False
-        self.x=0.5
-#         for j in range(5):
-#             self.x=self.x/2
         self.descente=1
     def deplacement(self):
         if btn(KEY_D) or btn(KEY_RIGHT):
             self.pos[0]+=5
         if btn(KEY_Q) or btn(KEY_LEFT):
             self.pos[0]-=5
-    def saut(self): #frame
+    def saut(self):
         self.s=True
         if self.h==0:
             self.h=self.pos[1]
-            for i in range (10):
-                print('1',self.h)
+            print('1',self.h)
+            for i in range (20):
+                print('5',self.h)
                 self.h-=2
         while self.pos[1]>self.h:
-            self.pos[1]-=self.x
+            self.pos[1]-=1
             print('2',self.pos[1])
         self.h=0
         self.s=False
