28149c3157f66d09e26c3221cdc9657f1c578cac
Elise <eliseromerogoux@hotmail.com>
2026-02-16 07:52:28 +0100

    Version avec arret sur ptf mais ne redescend pas

--- test1.py
+++ test1.py
@@ -3,6 +3,8 @@
 
 from pyxel import*
 from class_perso import*
+from class_plateforme import*
+from class_sol import*
 from time import*
 #RAJOUTER SOL
 class App:
@@ -10,6 +12,8 @@ class App:
         init(256, 256, title="Projet Libre")
         self.image=nom_fichier
         self.perso=Personnage(coord,1)
+        self.ptf=Plateforme([150,170])
+        self.s=Sol()
         run(self.update, self.draw)
     def update(self):
         if btn(KEY_D) or btn(KEY_RIGHT):
@@ -20,7 +24,7 @@ class App:
                 self.perso.pos[0]-=5
         if btnp(KEY_Z) or btnp(KEY_UP):
             self.perso.saut()
-        if self.perso.pos[1]<210: #ajouter or plateforme
+        if self.perso.pos[1]<self.s.sol and (self.perso.pos[1]+20!=self.ptf.co[1] and self.perso.pos[0]+20!=self.ptf.co[0] and self.perso.pos[0]!=self.ptf.co[0]): #ajouter or plateforme
             self.perso.pos[1]+=self.perso.descente
         print('4',self.perso.pos[1])
         #faire un test qui verifie si on est sur une plateforme, sinon on descend(gere la descente du saut et la gravité)
@@ -29,6 +33,6 @@ class App:
         load(self.image)
         cls(11)
         blt(self.perso.pos[0],self.perso.pos[1],0,0,0,32,32,13)
-        blt(10,10,0,0,32,64,16,13)
+        blt(self.ptf.co[0],self.ptf.co[1],0,0,32,64,16,13)
 
 App('res.pyxres',[0,210])
