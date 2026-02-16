58f2ff4f7a43276bfa7898bb98cb4ccb2cc2c03f
Elise <eliseromerogoux@hotmail.com>
2026-02-16 07:27:19 +0100

    Version 2 (saut et descente)

--- test1.py
+++ test1.py
@@ -4,19 +4,12 @@
 from pyxel import*
 from class_perso import*
 from time import*
-from class_plateforme import*
-from class_sol import*
-
-
 #RAJOUTER SOL
 class App:
     def __init__(self, nom_fichier, coord): #coord doit etre donné sous la forme [x,y]
         init(256, 256, title="Projet Libre")
         self.image=nom_fichier
         self.perso=Personnage(coord,1)
-        self.saute=0
-        self.ptf=Plateforme([150,170])
-        self.S=Sol()
         run(self.update, self.draw)
     def update(self):
         if btn(KEY_D) or btn(KEY_RIGHT):
@@ -26,16 +19,16 @@ class App:
             if self.perso.pos[0]-2>0:
                 self.perso.pos[0]-=5
         if btnp(KEY_Z) or btnp(KEY_UP):
-            self.saute+=1
-            if self.saute==1:
-                self.perso.saut()
-
+            self.perso.saut()
+        if self.perso.pos[1]<210: #ajouter or plateforme
+            self.perso.pos[1]+=self.perso.descente
+        print('4',self.perso.pos[1])
+        #faire un test qui verifie si on est sur une plateforme, sinon on descend(gere la descente du saut et la gravité)
+        # créer une valeur, qui est- la somme des forces qui nous font descendre et monter et appliquer cette force
     def draw(self):
         load(self.image)
         cls(11)
         blt(self.perso.pos[0],self.perso.pos[1],0,0,0,32,32,13)
-#         ATTENTION: on prend les coo en haut a gauche donc il faut calculer la diff pour faire
-#arriver le perso sur la plateforme
-        blt(self.ptf.co[0],self.ptf.co[1],0,0,32,64,16,13)
+        blt(10,10,0,0,32,64,16,13)
 
 App('res.pyxres',[0,210])
